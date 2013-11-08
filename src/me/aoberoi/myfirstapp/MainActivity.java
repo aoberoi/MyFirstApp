35ee0327cb7a185d1e8a0e8b40871f7c2db06376
Ankur Oberoi <aoberoi@gmail.com>
2013-11-08 14:54:24 -0500

    adds actionbar menu items

--- src/me/aoberoi/myfirstapp/MainActivity.java
+++ src/me/aoberoi/myfirstapp/MainActivity.java
@@ -1,9 +1,10 @@
 package me.aoberoi.myfirstapp;
 
-import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
@@ -19,8 +20,24 @@ public class MainActivity extends Activity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		getMenuInflater().inflate(R.menu.main_activity_actions, menu);
 		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle presses of the action bar items
+		switch(item.getItemId()) {
+			case R.id.action_search:
+				openSearch();
+				return true;
+			case R.id.action_settings:
+				openSettings();
+				return true;
+			default:
+				return super.onOptionsItemSelected(item);
+				
+		}
 	}
 	
 	/** Called when user clicks the Send button */
@@ -31,6 +48,14 @@ public class MainActivity extends Activity {
 		String message = editText.getText().toString();
 		intent.putExtra(EXTRA_MESSAGE, message);
 		startActivity(intent);
+	}
+	
+	private void openSearch() {
+		return;
+	}
+	
+	private void openSettings() {
+		return;
 	}
 
 }
